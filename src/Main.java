94e98a05e8894b459154ffb568bee745affd2f4f
edramos-97 <up201505779@fe.up.pt>
2018-12-06 13:38:15 +0000

    improve data export format to match the desired pattern

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,29 @@
 import java.io.*;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        if(args.length < 1) {
+        if (args.length != 1) {
             System.out.println("Please insert number of txt files...");
             return;
         }
 
         final String dir = System.getProperty("user.dir");
-
-        for(int i = 1; i <= Integer.valueOf(args[0]); i++) {
-        File fileTXT = new File(dir +"/txtFiles" +"/statFile" + i + ".txt");
-        File fileCSV =  new File(dir + "/csvFiles"+"/statFile" + i + ".csv");
-        readWriteToFile(fileTXT, fileCSV);
+        File fileCSV = Paths.get(dir, "csvFiles", "statFile.csv").toFile();
+        PrintWriter pw = new PrintWriter(fileCSV);
+        pw.print("RngSeed,DeathRate,ImmigrantChanceCooperateWithDifferent,ImmigrantChanceCooperateWithSame,ImmigrantsPerDay,InitialPtr,MutationRate,PlotResolution,SmartChoice,SpaceSize,TickDelay,CC,CD,DC,DD,CC-end,CD-end,DC-end,DD-end\n");
+        for (int i = 1; i <= Integer.valueOf(args[0]); i++) {
+            File fileTXT = Paths.get(dir, "txtFiles", "statFile" + i + ".txt").toFile();
+            readWriteToFile(fileTXT, pw);
         }
+        pw.close();
     }
 
-    public  static void readWriteToFile(File fileTXT, File fileCSV) throws IOException {
+    public static void readWriteToFile(File fileTXT, PrintWriter pw) throws IOException {
         FileInputStream fis = new FileInputStream(fileTXT);
-        OutputStream os = new FileOutputStream(fileCSV);
 
         //Construct BufferedReader from InputStreamReader
         BufferedReader br = new BufferedReader(new InputStreamReader(fis));
@@ -30,26 +33,32 @@ public class Main {
         int i = 0;
 
         while ((line = br.readLine()) != null) {
-
             if (i > 0 && i < 12) {
                 String d[] = line.split(": ");
-                String data = d[0] +","+d[1] + "\n";
-                os.write(data.getBytes(), 0, data.length());
+                if(i == 9){
+                    if(d[1].equals("false")){
+                        pw.print("0,");
+                    }else{
+                        pw.print("1,");
+                    }
+                }else{
+                    pw.print(d[1] + ",");
+                }
             }
 
             if (i == 14) {
+                System.out.println(line);
                 String d[] = line.split(", ");
-                String data = d[1]+","+d[2]+","+d[3]+","+d[4] + "\n";
-                os.write(data.getBytes(), 0, data.length());
+                pw.print(String.join(",", Arrays.copyOfRange(d, 1, 5)));
+                pw.print(',');
             }
 
             beforeLine = line;
             i++;
         }
-
+        System.out.println(beforeLine);
         String d[] = beforeLine.split(", ");
-        String data = d[1]+","+d[2]+","+d[3]+","+d[4] + "\n";
-        os.write(data.getBytes(), 0, data.length());
+        pw.println(String.join(",", Arrays.copyOfRange(d, 1, 5)));
         br.close();
     }
 }
